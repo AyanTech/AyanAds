2f1e474911856e33c80f0057a858f0032ef3dee4
Mohammad <mj.alijani@gmail.com>
2025-03-17 10:43:13 +0330

    added AdRequestConfigBuilder.kt

---
+++ ayanadmanager/src/main/java/ir/ayantech/ayanadmanager/model/AdRequestConfigBuilder.kt
@@ -0,0 +1,76 @@
+package ir.ayantech.ayanadmanager.model
+
+import android.app.Activity
+import android.view.ViewGroup
+import com.google.android.gms.ads.AdSize
+import ir.ayantech.ayanadmanager.core.AdCallback
+import ir.ayantech.ayanadmanager.model.api.AddStatisticsInputParameters
+import ir.ayantech.ayanadmanager.networks.hamrahAds.components.NativeAdAttributes
+import ir.ayantech.ayanadmanager.utils.ContainerType
+import ir.ayantech.ayanadmanager.utils.constant.AdSource
+import ir.ayantech.hamrahads.domain.enums.HamrahAdsBannerType
+
+class AdRequestConfigBuilder(
+    private val containerType: ContainerType,
+    private val activity: Activity,
+    private val addStatisticsInput: AddStatisticsInputParameters,
+    private val callback: AdCallback
+) {
+
+    private var hamrahAdSize: HamrahAdsBannerType? = null
+    private var googleAdSize: AdSize = AdSize.BANNER
+    private var viewGroup: ViewGroup? = null
+    private var nativeAdAttributes: NativeAdAttributes = NativeAdAttributes()
+    private var useDefaultNativeAdView: Boolean = false
+    private var adUnitId: String? = null
+
+    fun setHamrahAdConfig(
+        adSize: HamrahAdsBannerType?,
+        viewGroup: ViewGroup?,
+        nativeAdAttributes: NativeAdAttributes,
+        useDefaultNativeAdView: Boolean
+    ): AdRequestConfigBuilder {
+        this.hamrahAdSize = adSize
+        this.viewGroup = viewGroup
+        this.nativeAdAttributes = nativeAdAttributes
+        this.useDefaultNativeAdView = useDefaultNativeAdView
+        return this
+    }
+
+    fun setAdMobConfig(adUnitId: String, viewGroup: ViewGroup?): AdRequestConfigBuilder {
+        this.viewGroup = viewGroup
+        this.adUnitId = adUnitId
+        return this
+    }
+
+    fun build(adSource: AdSource): AdRequestConfig {
+        return when (adSource) {
+            AdSource.HamrahAd -> HamrahAdConfig(
+                adSize = hamrahAdSize,
+                viewGroup = viewGroup,
+                nativeAdAttributes = nativeAdAttributes,
+                useDefaultNativeAdView = useDefaultNativeAdView,
+                containerType = containerType,
+                activity = activity,
+                addStatisticsInput = addStatisticsInput,
+                callback = callback
+            )
+
+            AdSource.Adivery -> TODO()
+            AdSource.AdMob -> AdMobConfig(
+                containerType = containerType,
+                activity = activity,
+                viewGroup = viewGroup,
+                addStatisticsInput = addStatisticsInput,
+                adUnitId = adUnitId ?: "",
+                callback = callback,
+                adSize = googleAdSize,
+                useDefaultNativeAdView = useDefaultNativeAdView
+            )
+
+            AdSource.Tapsell -> TODO()
+        }
+    }
+
+
+}
