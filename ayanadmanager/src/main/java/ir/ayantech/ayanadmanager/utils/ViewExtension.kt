31ba7d098da255660d53175a4e12a5dedebf4a5a
Mohammad <mj.alijani@gmail.com>
2025-03-17 10:44:59 +0330

    added more extension function

--- ayanadmanager/src/main/java/ir/ayantech/ayanadmanager/utils/ViewExtension.kt
+++ ayanadmanager/src/main/java/ir/ayantech/ayanadmanager/utils/ViewExtension.kt
@@ -1,7 +1,16 @@
 package ir.ayantech.ayanadmanager.utils
 
 import android.content.Context
+import android.view.View
 
 fun Int.toPx(context: Context): Int {
     return (this * context.resources.displayMetrics.density).toInt()
+}
+
+fun View.makeVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.makeGone() {
+    this.visibility = View.GONE
 }
